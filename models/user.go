5dcae4182d883b7dfaa3b451f49a18dc4d62947f
Eren Yusuf Duran <eren.duran@colins.com>
2024-02-08 18:19:50 +0300

    hashing passwords

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/erenyusufduran/rest-api/db"
+import (
+	"github.com/erenyusufduran/rest-api/db"
+	"github.com/erenyusufduran/rest-api/utils"
+)
 
 type User struct {
 	ID       int64
@@ -16,7 +19,12 @@ func (u User) Save() error {
 	}
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
