872361a0c7aa379f5ae9fb11d93d5e64c1666029
Eren Yusuf Duran <eren.duran@colins.com>
2024-02-11 14:21:34 +0300

    retrieving and storing user and event ids

--- routes/events.go
+++ routes/events.go
@@ -39,7 +39,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	err := utils.VerifyToken(token)
+	userId, err := utils.VerifyToken(token)
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
 		return
@@ -52,7 +52,7 @@ func createEvent(context *gin.Context) {
 		return
 	}
 
-	event.UserID = 1
+	event.UserID = userId
 
 	err = event.Save()
 	if err != nil {
--- utils/jwt.go
+++ utils/jwt.go
@@ -26,7 +26,7 @@ func GenerateToken(email string, userId int64) (string, error) {
 	return token.SignedString(privKey)
 }
 
-func VerifyToken(token string) error {
+func VerifyToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
 		// _, ok := t.Method.(*jwt.SigningMethodHMAC)
 		// if !ok {
@@ -40,21 +40,21 @@ func VerifyToken(token string) error {
 	})
 
 	if err != nil {
-		return errors.New("could not parse token")
+		return 0, errors.New("could not parse token")
 	}
 
 	tokenIsValid := parsedToken.Valid
 	if !tokenIsValid {
-		return errors.New("invalid token")
+		return 0, errors.New("invalid token")
 	}
 
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 
-	// if !ok {
-	// 	return errors.New("invalid token claims")
-	// }
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
 
 	// email := claims["email"].(string)
-	// userId := claims["userId"].(int64)
-	return nil
+	userId := int64(claims["userId"].(float64))
+	return userId, nil
 }
