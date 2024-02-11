7b3f72d3889c9d8d2d1bd4a82b84b4eeeecb7751
Eren Yusuf Duran <eren.duran@colins.com>
2024-02-11 14:38:27 +0300

    middleware usage

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"github.com/erenyusufduran/rest-api/models"
-	"github.com/erenyusufduran/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -33,26 +32,20 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		return
 	}
 
-	event.UserID = userId
+	userId, exists := context.Get("userId")
+	if !exists {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not resolve user."})
+		return
+	}
+
+	event.UserID = userId.(int64)
 
 	err = event.Save()
 	if err != nil {
